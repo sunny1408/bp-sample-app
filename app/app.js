ed569c2f977376d72e8373caa6c49a2a81ea5b39
Koushik <sunnys1408@gmail.com>
2016-08-07 19:17:33 -0500

    changes to fix console errors

--- app/app.js
+++ app/app.js
@@ -1,29 +1,27 @@
 (function() {
-	var app = angular.module('app', ['ui.router', 'ui.bootstrap']);
- 
-	app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
-		// For any unmatched url, redirect to /state1
-		$urlRouterProvider.otherwise("/inbox");
-		//$locationProvider.html5Mode(true);
- 
-		// Now set up the states
-		$stateProvider
-			.state('inbox', {
-				url: "/inbox",
-				templateUrl: "/app/inbox/inbox.template.html",
-				controller: 'InboxController as vm'
-			})
+    var app = angular.module('app', ['ui.router', 'ui.bootstrap']);
 
-			.state('inbound', {
-			 url: "/inbound",
-			 templateUrl: "/app/inbound/inbound.template.html",
-			 controller: 'InboundController as vm'
-			})
-			
-			.state('outbound', {
-			 url: "/outbound",
-			 templateUrl: "/app/outbound/outbound.template.html",
-			 controller: 'OutboundController as vm'
-		});
-	});
-)()
+    app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/inbox");
+        //$locationProvider.html5Mode(true);
+
+        // Now set up the states
+        $stateProvider
+            .state('inbox', {
+                url: "/inbox",
+                templateUrl: "/app/inbox/inbox.template.html",
+                controller: 'InboxController as vm'
+            })
+            .state('inbound', {
+                url: "/inbound",
+                templateUrl: "/app/inbound/inbound.template.html",
+                controller: 'InboundController as vm'
+            })
+            .state('outbound', {
+                url: "/outbound",
+                templateUrl: "/app/outbound/outbound.template.html",
+                controller: 'OutboundController as vm'
+            });
+    });
+})()
